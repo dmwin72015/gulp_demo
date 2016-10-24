b4c9003900bd5e5fcc6f6bfef8680854e4ac092c
marourou2016@icloud.com <marourou2016@icloud.com>
2016-10-24 19:10:35 +0800

    test

--- plugin/model-conact.js
+++ plugin/model-conact.js
@@ -1,21 +1,51 @@
 'use strict';
 var through = require('through2');
 var gutil = require('gulp-util');
-var PluginError = gutil.PluginError;
 var pp = require('preprocess');
 
+var PluginError = gutil.PluginError;
+
+var whitespace = "[\\x20\\t\\r\\n\\f]";
+
 function getIncludeLocation() {
-	
+
 
 
 }
 
 
-function dm_concat(obj) {
+function dm_concat(options) {
+  console.log(this);
+  return through.obj(function(file, enc, cb) {
+    console.log(file);
 
-  console.log(obj)
+    console.log(typeof file);
+    // streamToString(file, (err, str) => {
+    //   console.log(str);
+    // })
 
-  return
+    console.log('\n' + enc);
+    console.log('\n' + cb);
+  });
 }
 
+
+function streamToString(stream, cb) {
+  stream.on('readable', function() {
+    var read = stream.read();
+    if (read !== null) {
+      output += read.toString();
+    } else {
+      cb(null, output);
+    }
+  });
+}
 module.exports = dm_concat;
+module.exports.dd = 12;
+
+module.exports.bb = {
+  a:12
+};
+
+
+
--- test/demo.js
+++ test/demo.js
@@ -1,4 +1,35 @@
-process.stdin.on('readable', function () {
-    var buf = process.stdin.read();
-    console.dir(buf);
-});
+const stream = require('stream');
+const util = require('util');
+const through = require('through2');
+
+const test = require('../plugin/model-conact.js')
+
+const reader = stream.Readable;
+const writer = stream.Writable;
+
+util.inherits(Counter, reader);
+
+function Counter(opt) {
+  reader.call(this, opt);
+  this._max = 1000000;
+  this._index = 1;
+}
+
+Counter.prototype._read = function() {
+  var i = this._index++;
+  if (i > this._max)
+    this.push(null);
+  else {
+    var str = '' + i;
+    var buf = new Buffer(str, 'ascii');
+    this.push(buf);
+  }
+};
+Counter.prototype.toString = function(){
+	return this.buffer.toString();
+}
+var c1 = new Counter;
+var c2 = new Counter;
+c1.push('中国');
+c1.push('帝都');
+console.log(c1.toString());
